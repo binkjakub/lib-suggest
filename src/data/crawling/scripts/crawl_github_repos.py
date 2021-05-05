ddb0e12f3c25dfe50b6d3e90d1a2db6b8dba3e70
albert.sawczyn <albertsawczyn@gmail.com>
2021-05-05 23:12:33 +0200

    add scheduler

--- src/data/crawling/scripts/crawl_github_repos.py
+++ src/data/crawling/scripts/crawl_github_repos.py
@@ -5,8 +5,9 @@ the Repository object and saves it to json.
 import multiprocessing
 import queue
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from multiprocessing import Process
+from random import randrange
 from typing import Optional
 
 import click
@@ -16,7 +17,7 @@ from src.feature_extraction.repo import extract_batch
 
 
 @click.command()
-@click.option('--access-token', type=click.STRING, required=True,
+@click.option('--access-token', '-a', type=click.STRING, required=True,
               help="Access token generated with GitHub account or None in case of using public API"
                    "(limits are stricter in case of public API)")
 @click.option('--query', type=click.STRING, default="language:python")
@@ -25,13 +26,42 @@ from src.feature_extraction.repo import extract_batch
 @click.option('--step-day', type=click.INT, default=10)
 @click.option('--daily-limit', type=click.INT, default=10)
 @click.option('--n-jobs', type=click.INT, default=1)
+@click.option('--repeat-span', type=click.INT, default=30)
 def crawl_github_repos(access_token: str,
                        query: str,
                        start_date: datetime,
                        end_date: datetime,
                        step_day: int,
                        daily_limit: int,
-                       n_jobs: int):
+                       n_jobs: int,
+                       repeat_span: int):
+    while True:
+        random_start_date = random_date(start_date, end_date)
+        random_end_date = random_date(random_start_date, end_date)
+
+        print(random_start_date, '-', random_end_date)
+        execute(access_token, query, start_date, end_date, step_day, daily_limit, n_jobs)
+        time.sleep(repeat_span * 60)
+
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+
+def execute(access_token: str,
+            query: str,
+            start_date: datetime,
+            end_date: datetime,
+            step_day: int,
+            daily_limit: int,
+            n_jobs: int):
     repos_queue = multiprocessing.Queue()
     args = (repos_queue, access_token, query, start_date, end_date, step_day, daily_limit)
     search_proc = Process(target=_worker_repo_list_scraper, args=args)
--- src/defaults.py
+++ src/defaults.py
@@ -4,4 +4,4 @@ from pathlib import Path
 DVC_PATH = Path(os.path.dirname(__file__)).parent.joinpath('storage').absolute()
 DATASET_PATH = DVC_PATH / 'dataset'
 
-REPOS = DATASET_PATH / 'scraped_repos.jsonl'
+REPOS = DATASET_PATH / f"scraped_repos_{os.environ['USER']}.jsonl"
