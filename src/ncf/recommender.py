58ac32d65a0810cfd6addd224629ca8aee3987cd
Jakub Binkowski <binkjakub@gmail.com>
2021-06-15 18:23:58 +0200

    feat: Add manual features for repository

--- src/ncf/recommender.py
+++ src/ncf/recommender.py
@@ -26,9 +26,7 @@ class RecommenderSystem(LightningModule, ABC):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
     def training_step(self, train_batch, batch_id):
-        repos, libs, ratings = train_batch
-        ratings_pred = self.forward(repos, libs).squeeze(-1)
-
+        repos, _, ratings, _, ratings_pred = self.shared_step(train_batch)
         loss = F.binary_cross_entropy(ratings_pred, ratings)
         self.log('train/loss', loss)
 
@@ -37,13 +35,23 @@ class RecommenderSystem(LightningModule, ABC):
         return loss
 
     def validation_step(self, val_batch, batch_id):
-        repos, libs, ratings = val_batch
-        ratings_pred = self.forward(repos, libs).squeeze(-1)
+        repos, _, ratings, _, ratings_pred = self.shared_step(val_batch)
 
         loss = F.binary_cross_entropy(ratings_pred, ratings)
         self.log('val/loss', loss)
 
         self.val_metrics(ratings_pred, ratings.long(), indexes=repos.long())
+
+    def shared_step(self, input_batch):
+        if len(input_batch) == 4:
+            repos, libs, ratings, user_features = input_batch
+        elif len(input_batch) == 3:
+            repos, libs, ratings = input_batch
+            user_features = None
+        else:
+            raise ValueError(f"Invalid size of input tuple: {len(input_batch)}")
+        ratings_pred = self.forward(repos, libs, user_features).squeeze(-1)
+        return repos, libs, ratings, user_features, ratings_pred
 
     def training_epoch_end(self, outputs):
         self._aggregate_and_log_metrics(self.train_metrics)
--- src/ncf/scripts/run_training.py
+++ src/ncf/scripts/run_training.py
@@ -14,32 +14,32 @@ from src.ncf.recommender import RecommenderSystem
               help="Directory where logs will be stored")
 @click.option('--experiment-name', type=click.STRING, default='ncf',
               help="Name of experiment passed to the logger")
-@click.option('--seed', type=click.INT, default=2137,
+@click.option('--seed', type=click.INT, default=2137123,
               help="Random seed for reproducibility")
 def run_ncf_training(log_dir: str, experiment_name: str, seed: int):
-    embedding_dim = 32
-    combination_layer_dim = embedding_dim * 2
     config = {
         'model': 'mlp',
+        'embedding_dim': 16,
+        'manual_feat_dim': len(LibRecommenderDM.FEATURE_NAMES),
+        # 'manual_feat_dim': 0,
+        'manual_feat_combination_out_dim': 0,
         'num_repos': 1016,
         'num_libs': 401,
-        'num_negatives': 8,
+        'num_negatives': 4,
         'batch_size': 128,
         'num_workers': 0,
         'learning_rate': 1e-3,
-        'latent_dim_mf': embedding_dim,
-        'latent_dim_mlp': embedding_dim,
-        'layers': [combination_layer_dim, 32, 8],
-        'max_epochs': 5,
+        'max_epochs': 32,
         'k_eval': 5,
         'seed': seed,
     }
+    config = _compute_dimensions(config)
     seed_everything(seed)
 
     book_crossing_dm = LibRecommenderDM(config)
     model = _get_model(config)
 
-    experiment_name = f'test__{type(model).__name__}__{experiment_name}'
+    experiment_name = f'{type(model).__name__}__{experiment_name}'
     trainer = Trainer(
         max_epochs=config['max_epochs'],
         logger=_get_loggers(log_dir, experiment_name),
@@ -47,6 +47,25 @@ def run_ncf_training(log_dir: str, experiment_name: str, seed: int):
         deterministic=True,
     )
     trainer.fit(model, datamodule=book_crossing_dm)
+
+
+def _compute_dimensions(config: dict[str, Any]) -> dict[str, Any]:
+    if config['manual_feat_combination_out_dim']:  # when using combination layer after feat concat
+        assert config['manual_feat_dim']
+        repo_embedding_dim = config['manual_feat_combination_out_dim']
+    elif config['manual_feat_dim']:  # when using only feat concat
+        repo_embedding_dim = config['manual_feat_dim'] + config['embedding_dim']
+    else:
+        repo_embedding_dim = config['embedding_dim']
+
+    lib_embedding_dim = config['embedding_dim']
+
+    concat_dim = repo_embedding_dim + lib_embedding_dim
+    config |= {
+        'latent_dim_mf': None,
+        'layers': [concat_dim, 32, 8],
+    }
+    return config
 
 
 def _get_loggers(log_dir: str, experiment_name: str) -> LightningLoggerBase:
